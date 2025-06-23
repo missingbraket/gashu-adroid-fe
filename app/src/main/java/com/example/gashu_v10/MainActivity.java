9acaad188b9bdc09e14057b19d727dd0422191fe
seohallo <seoohaii@gmail.com>
2025-06-24 03:58:15 +0900

    feat:전화연결기능추가

--- app/src/main/java/com/example/gashu_v10/MainActivity.java
+++ app/src/main/java/com/example/gashu_v10/MainActivity.java
@@ -1,39 +1,62 @@
 package com.example.gashu_v10;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView recordBtn, placeBtn;
+    private static final int REQUEST_CALL_PHONE = 1;
+
+    private ImageView recordBtn, callBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         recordBtn = findViewById(R.id.recordBtn);
-        placeBtn = findViewById(R.id.placeBtn);
+        callBtn = findViewById(R.id.callBtn);
 
-        recordBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RecordActivity.class);
-                startActivity(intent);
-            }
+        recordBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RecordActivity.class);
+            startActivity(intent);
         });
 
-        placeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PlaceActivity.class);
-                startActivity(intent);
-            }
+        callBtn.setOnClickListener(v -> {
+            makeEmergencyCall();
         });
+    }
+
+    private void makeEmergencyCall() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:112"));
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // 전화 권한
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE);
+        } else {
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_CALL_PHONE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makeEmergencyCall();
+            } else {
+                Toast.makeText(this, "전화 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
